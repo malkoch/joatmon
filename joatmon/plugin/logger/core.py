4445ebbea4b1f1ed20b69752b37a6608ab7ddb14
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-02-22 16:08:40 +0300

    added log plugin

---
+++ joatmon/plugin/logger/core.py
@@ -0,0 +1,59 @@
+from typing import Union
+
+from joatmon.orm.enum import Enum
+from joatmon.plugin.core import Plugin
+
+
+class LogLevel(Enum):
+    NotSet = 1 << 0
+    Debug = 1 << 1
+    Info = 1 << 2
+    Warning = 1 << 3
+    Error = 1 << 4
+    Critical = 1 << 5
+
+
+class LoggerPlugin(Plugin):
+    def __init__(self, level):
+        if isinstance(level, LogLevel):
+            level = level.name
+        self._level = LoggerPlugin._get_level(level)
+
+    @staticmethod
+    def _get_level(level_str):
+        _level_key = list(filter(lambda x: level_str.lower() == x.lower(), LogLevel.__dict__.keys()))
+        if len(_level_key) == 1:
+            _level_key = _level_key[0]
+        else:
+            _level_key = LogLevel.Info.name
+
+        return LogLevel[_level_key]
+
+    async def _write(self, log: dict):
+        raise NotImplementedError
+
+    async def log(self, log: dict, level: Union[LogLevel, str] = LogLevel.Debug):
+        try:
+            if isinstance(level, str):
+                level = LoggerPlugin._get_level(level)
+
+            if self._level.value <= level.value:
+                log['level'] = level.name
+                await self._write(log)
+        except Exception as ex:
+            print(str(ex))
+
+    async def debug(self, log):
+        await self.log(log=log, level=LogLevel.Debug)
+
+    async def info(self, log):
+        await self.log(log=log, level=LogLevel.Info)
+
+    async def warning(self, log):
+        await self.log(log=log, level=LogLevel.Warning)
+
+    async def error(self, log):
+        await self.log(log=log, level=LogLevel.Error)
+
+    async def critical(self, log):
+        await self.log(log=log, level=LogLevel.Critical)
---
+++ joatmon/plugin/logger/file.py
@@ -0,0 +1,21 @@
+import datetime
+import json
+import os
+
+from joatmon.plugin.logger.core import LoggerPlugin
+
+
+class FileLogger(LoggerPlugin):
+    def __init__(self, level: str, base_folder: str):
+        super(FileLogger, self).__init__(level)
+
+        self.folder = base_folder
+
+    async def _write(self, log: dict):
+        dt = datetime.datetime.now()
+        folder = os.path.join(self.folder, dt.strftime(f'%Y{os.sep}%m{os.sep}'))
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        file = os.path.join(folder, dt.strftime(f'%d') + '.log')
+        with open(file, 'a') as f:
+            f.write(json.dumps(log) + os.linesep)
