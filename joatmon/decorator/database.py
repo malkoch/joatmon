2300c7ebe3ca645cca4b5f79b667e053a4b2bf71
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-02-22 16:13:18 +0300

    added database decorator

---
+++ joatmon/decorator/database.py
@@ -0,0 +1,25 @@
+import functools
+import inspect
+
+import async_exit_stack
+
+from core import ctx
+
+
+def transaction(_func=None, names=None):
+    def _decorator(func):
+        @functools.wraps(func)
+        async def _wrapper(*args, **kwargs):
+            async with async_exit_stack.AsyncExitStack() as stack:
+                for database in names or []:
+                    await stack.enter_async_context(ctx.get_value(database))
+
+                return await func(*args, **kwargs)
+
+        _wrapper.__signature__ = inspect.signature(func)
+        return _wrapper
+
+    if _func is None:
+        return _decorator
+    else:
+        return _decorator(_func)
