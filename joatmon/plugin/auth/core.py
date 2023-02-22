3cae7db046443c2f4893759e1ca4cff5e5528158
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-02-22 18:01:08 +0300

    auth plugin fixed

--- joatmon/plugin/auth/core.py
+++ joatmon/plugin/auth/core.py
@@ -2,7 +2,7 @@ from joatmon.plugin.core import Plugin
 
 
 class Auth(Plugin):
-    async def authenticate(self, user_id, issuer, audience, expire_at):
+    async def authenticate(self, issuer, audience, expire_at):
         raise NotImplementedError
 
     async def authorize(self, token, issuer, audience, min_role):
