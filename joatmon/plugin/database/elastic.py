7a25910990111951a5d9c1d1c187b8a94c0c431b
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-03-12 16:39:03 +0300

    update elastic

--- joatmon/plugin/database/elastic.py
+++ joatmon/plugin/database/elastic.py
@@ -5,6 +5,7 @@ from elasticsearch import Elasticsearch
 
 from joatmon import context
 from joatmon.orm.document import Document
+from joatmon.orm.meta import normalize_kwargs
 from joatmon.plugin.database.core import DatabasePlugin
 
 
@@ -29,7 +30,7 @@ class ElasticDatabase(DatabasePlugin):
 
         dictionary = document.validate()
 
-        self.client.index(index=document.__metaclass__.__collection__, document=dictionary)
+        self.client.index(index=document.__metaclass__.__collection__, id=dictionary['object_id'], document=dictionary)
 
         return document
 
@@ -46,16 +47,45 @@ class ElasticDatabase(DatabasePlugin):
         return documents
 
     async def read(self, document, **kwargs):
-        ...
+        result = self.client.get(index=document.__metaclass__.__collection__, document=normalize_kwargs(document.__metaclass__, **kwargs))
+
+        for doc in result:
+            yield document(**doc)
 
     async def update_raw(self, document):
-        ...
+        if not isinstance(document, Document):
+            raise ValueError(f'{type(document)} is not valid for saving')
+
+        dictionary = document.validate()
+
+        self.client.update(index=document.__metaclass__.__collection__, id=dictionary['object_id'], document=dictionary)
+
+        return document
 
     async def update(self, *documents):
-        ...
+        for document in documents:
+            user = context.get_value(self.user_plugin).get()
+            document.creator_id = user.object_id if user is not None else uuid.UUID(int=0)
+            document.created_at = datetime.utcnow()
+            document.updater_id = user.object_id if user is not None else uuid.UUID(int=0)
+            document.updated_at = datetime.utcnow()
+
+            await self.update_raw(document)
+
+        return documents
 
     async def delete(self, *documents):
-        ...
+        for document in documents:
+            user = context.get_value(self.user_plugin).get()
+            document.updater_id = user.object_id if user is not None else uuid.UUID(int=0)
+            document.updated_at = datetime.utcnow()
+            document.deleter_id = user.object_id if user is not None else uuid.UUID(int=0)
+            document.deleted_at = datetime.utcnow()
+            document.is_deleted = True
+
+            await self.update_raw(document)
+
+        return documents
 
     async def start(self):
         ...
