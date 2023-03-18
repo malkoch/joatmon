7f8711110b54b89391d62a5606cd28a7b8da2f50
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-03-18 16:09:00 +0300

    implementing couchbase methods

--- joatmon/plugin/database/couchbase.py
+++ joatmon/plugin/database/couchbase.py
@@ -2,8 +2,11 @@ import uuid
 
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
-from couchbase.options import ClusterOptions
-from joatmon.core.utility import to_enumerable
+from couchbase.logic.n1ql import QueryScanConsistency
+from couchbase.options import (
+    ClusterOptions,
+    QueryOptions
+)
 
 from joatmon.plugin.database.core import DatabasePlugin
 
@@ -31,18 +34,52 @@ class CouchBaseDatabase(DatabasePlugin):
         for doc in docs:
             await self._create_collection(document.__metaclass__)
 
-            self.db.collection(document.__metaclass__.__collection__).insert(str(uuid.uuid4()), to_enumerable(doc, string=True))
+            values = '{' + ', '.join([f'"{k}" : ${k}' for k, v in doc.items()]) + '}'
+            self.db.query(
+                f'insert into `{self.bucket}`.{self.scope}.{document.__metaclass__.__collection__} '
+                f'(key, value) '
+                f'values '
+                f'(\'{str(uuid.uuid4())}\', {values}) '
+                f'returning *',
+                QueryOptions(named_parameters={k: str(v) if isinstance(v, uuid.UUID) else v for k, v in doc.items()}, scan_consistency=QueryScanConsistency.REQUEST_PLUS)
+            ).execute()
 
             yield document(**doc)
 
     async def read(self, document, query):
-        raise NotImplementedError
+        await self._create_collection(document.__metaclass__)
+
+        values = 'and '.join([f'`{k}` = ${k}' for k, v in query.items()])
+        result = self.db.query(
+            f'SELECT * '
+            f'FROM `{self.bucket}`.{self.scope}.{document.__metaclass__.__collection__} '
+            f'WHERE {values}',
+            QueryOptions(named_parameters={k: str(v) if isinstance(v, uuid.UUID) else v for k, v in query.items()}, scan_consistency=QueryScanConsistency.REQUEST_PLUS)
+        ).execute()
+        for r in result:
+            yield document(**r[document.__metaclass__.__collection__])
 
     async def update(self, document, query, update):
-        raise NotImplementedError
+        query_values = 'and '.join([f'`{k}` = $query_{k}' for k, v in query.items()])
+        update_values = ', '.join([f'`{k}` = $update_{k}' for k, v in update.items()])
+
+        params = {}
+        params.update({f'query_{k}': str(v) if isinstance(v, uuid.UUID) else v for k, v in query.items()})
+        params.update({f'update_{k}': str(v) if isinstance(v, uuid.UUID) else v for k, v in update.items()})
+        self.db.query(
+            f'update `{self.bucket}`.{self.scope}.{document.__metaclass__.__collection__} '
+            f'set {update_values} '
+            f'WHERE {query_values}',
+            QueryOptions(named_parameters=params, scan_consistency=QueryScanConsistency.REQUEST_PLUS)
+        ).execute()
 
     async def delete(self, document, query):
-        raise NotImplementedError
+        values = 'and '.join([f'`{k}` = ${k}' for k, v in query.items()])
+        self.db.query(
+            f'delete from `{self.bucket}`.{self.scope}.{document.__metaclass__.__collection__} '
+            f'WHERE {values}',
+            QueryOptions(named_parameters={k: str(v) if isinstance(v, uuid.UUID) else v for k, v in query.items()}, scan_consistency=QueryScanConsistency.REQUEST_PLUS)
+        ).execute()
 
     async def start(self):
         raise NotImplementedError
