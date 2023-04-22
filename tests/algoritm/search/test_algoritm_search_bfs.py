8c4294d660b61b3237e978ac46c84882ed8ede01
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-22 23:46:10 +0300

    Update test_algoritm_search_bfs.py

--- tests/algoritm/search/test_algoritm_search_bfs.py
+++ tests/algoritm/search/test_algoritm_search_bfs.py
@@ -1,12 +1,28 @@
+import collections
+
 import pytest
 
 
 def test_bfs():
-    assert True is True
+    from joatmon.algoritm.search.bfs import bfs
+    from joatmon.structure.graph import Graph
+
+    g = Graph([(0, 1), (1, 2)], 3)
+
+    ret = list(bfs(g, 0, [False, False, False]))
+    assert ret == [0, 1, 2]
 
 
 def test_bfs_r():
-    assert True is True
+    from joatmon.algoritm.search.bfs import bfs_r
+    from joatmon.structure.graph import Graph
+
+    g = Graph([(0, 1), (1, 2)], 3)
+    q = collections.deque()
+    q.append(0)
+
+    ret = list(bfs_r(g, q, [False, False, False]))
+    assert ret == [0, 1, 0, 2]
 
 
 if __name__ == '__main__':
