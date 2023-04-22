dd47d0444c7979dbd7f44bc2a6a95dbce0705ee6
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-22 23:52:04 +0300

    Update test_algoritm_search_linear.py

--- tests/algoritm/search/test_algoritm_search_linear.py
+++ tests/algoritm/search/test_algoritm_search_linear.py
@@ -2,7 +2,9 @@ import pytest
 
 
 def test_linear_search():
-    assert True is True
+    from joatmon.algoritm.search.linear import linear_search
+
+    assert linear_search([0, 1, 2, 3], 1, lambda x: x) == 1
 
 
 if __name__ == '__main__':
