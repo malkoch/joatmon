1c32f3d4cc6e6ee2fb0d8adcf878b68aff08bfd3
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-22 23:49:06 +0300

    Update test_algoritm_search_binary.py

--- tests/algoritm/search/test_algoritm_search_binary.py
+++ tests/algoritm/search/test_algoritm_search_binary.py
@@ -2,7 +2,9 @@ import pytest
 
 
 def test_binary_search():
-    assert True is True
+    from joatmon.algoritm.search.binary import binary_search
+
+    assert binary_search([0, 1, 2, 3], 1, lambda x: x) == 1
 
 
 def test_binary_search_helper():
@@ -10,7 +12,9 @@ def test_binary_search_helper():
 
 
 def test_binary_search_r():
-    assert True is True
+    from joatmon.algoritm.search.binary import binary_search_r
+
+    assert binary_search_r([0, 1, 2, 3], 1, lambda x: x) == 1
 
 
 if __name__ == '__main__':
