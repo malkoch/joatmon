9ee0e9ca579d1671e5a6acba12f0a102448a17b0
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-23 00:01:37 +0300

    Update test_algoritm_traverse_inorder.py

--- tests/algoritm/traverse/test_algoritm_traverse_inorder.py
+++ tests/algoritm/traverse/test_algoritm_traverse_inorder.py
@@ -2,11 +2,25 @@ import pytest
 
 
 def test_inorder():
-    assert True is True
+    from joatmon.algoritm.traverse.inorder import inorder
+    from joatmon.structure.tree import Tree
+
+    root = Tree(3)
+    root.left = Tree(1)
+    root.right = Tree(2)
+
+    assert list(inorder(root)) == [1, 3, 2]
 
 
 def test_inorder_r():
-    assert True is True
+    from joatmon.algoritm.traverse.inorder import inorder_r
+    from joatmon.structure.tree import Tree
+
+    root = Tree(3)
+    root.left = Tree(1)
+    root.right = Tree(2)
+
+    assert list(inorder_r(root)) == [3]
 
 
 if __name__ == '__main__':
