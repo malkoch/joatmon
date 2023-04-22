61820ede874fd3f49281e2de1a49201b286d5813
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-23 00:03:05 +0300

    Update test_algoritm_traverse_postorder.py

--- tests/algoritm/traverse/test_algoritm_traverse_postorder.py
+++ tests/algoritm/traverse/test_algoritm_traverse_postorder.py
@@ -2,11 +2,25 @@ import pytest
 
 
 def test_postorder():
-    assert True is True
+    from joatmon.algoritm.traverse.postorder import postorder
+    from joatmon.structure.tree import Tree
+
+    root = Tree(3)
+    root.left = Tree(1)
+    root.right = Tree(2)
+
+    assert list(postorder(root)) == [1, 2, 3]
 
 
 def test_postorder_r():
-    assert True is True
+    from joatmon.algoritm.traverse.postorder import postorder_r
+    from joatmon.structure.tree import Tree
+
+    root = Tree(3)
+    root.left = Tree(1)
+    root.right = Tree(2)
+
+    assert list(postorder_r(root)) == [3]
 
 
 if __name__ == '__main__':
