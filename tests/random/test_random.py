88223c93484d85d6826f2859e66875db940f73c9
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 23:57:17 +0300

    writing random tests

--- tests/random/test_random.py
+++ tests/random/test_random.py
@@ -2,40 +2,70 @@ import pytest
 
 
 def test_core_random_decay():
-	assert True is True
+    assert True is True
 
 
 def test_core_random_reset():
-	assert True is True
+    assert True is True
 
 
 def test_core_random_sample():
-	assert True is True
+    assert True is True
 
 
 def test_gaussian_random_decay():
-	assert True is True
+    from joatmon.random import GaussianRandom
+
+    random = GaussianRandom()
+    random.decay()
+
+    assert True is True
 
 
 def test_gaussian_random_reset():
-	assert True is True
+    from joatmon.random import GaussianRandom
+
+    random = GaussianRandom()
+    random.reset()
+
+    assert True is True
 
 
 def test_gaussian_random_sample():
-	assert True is True
+    from joatmon.random import GaussianRandom
+
+    random = GaussianRandom()
+    random.sample()
+
+    assert True is True
 
 
 def test_ornstein_uhlenbeck_decay():
-	assert True is True
+    from joatmon.random import OrnsteinUhlenbeck
+
+    random = OrnsteinUhlenbeck()
+    random.decay()
+
+    assert True is True
 
 
 def test_ornstein_uhlenbeck_reset():
-	assert True is True
+    from joatmon.random import OrnsteinUhlenbeck
+
+    random = OrnsteinUhlenbeck()
+    random.reset()
+
+    assert True is True
 
 
 def test_ornstein_uhlenbeck_sample():
-	assert True is True
+    from joatmon.random import OrnsteinUhlenbeck
+
+    random = OrnsteinUhlenbeck()
+    random.sample()
+
+    assert True is True
 
 
 if __name__ == '__main__':
-	pytest.main([__file__])
+    pytest.main([__file__])
