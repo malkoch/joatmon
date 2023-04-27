985f1319677a1fb848ae1c1a1ff2667bd32108a0
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 20:55:11 +0300

    writing processor tests

--- tests/ai/test_ai_processor.py
+++ tests/ai/test_ai_processor.py
@@ -2,12 +2,32 @@ import pytest
 
 
 def test_r_l_processor_process_batch():
-	assert True is True
+    import numpy as np
+    from joatmon.ai.processor import RLProcessor
+
+    processor = RLProcessor()
+    processor.process_batch(
+        [(
+            np.random.random((1, 3, 84, 84)),
+            np.random.random((1, 1)),
+            np.random.random((1, 1)),
+            np.random.random((1, 3, 84, 84)),
+            np.random.random((1, 1))
+        )]
+    )
+
+    assert True is True
 
 
 def test_r_l_processor_process_state():
-	assert True is True
+    import numpy as np
+    from joatmon.ai.processor import RLProcessor
+
+    processor = RLProcessor()
+    processor.process_state(np.random.randint(0, 255, (84, 84, 3)))
+
+    assert True is True
 
 
 if __name__ == '__main__':
-	pytest.main([__file__])
+    pytest.main([__file__])
