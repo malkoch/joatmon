c543b6cc611869290704c2264581c012cc30dc3c
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 23:39:22 +0300

    writing context tests

--- tests/context/test_context.py
+++ tests/context/test_context.py
@@ -2,18 +2,49 @@ import pytest
 
 
 def test_get_ctx():
+    from joatmon import context
+
+    context.get_ctx()
+
     assert True is True
 
 
 def test_get_value():
+    from joatmon import context
+
+    class C:
+        ...
+
+    ctx = C()
+    context.set_ctx(ctx)
+    context.set_value('a', 1)
+    context.get_value('a')
+
     assert True is True
 
 
 def test_set_ctx():
+    from joatmon import context
+
+    class C:
+        ...
+
+    ctx = C()
+    context.set_ctx(ctx)
+
     assert True is True
 
 
 def test_set_value():
+    from joatmon import context
+
+    class C:
+        ...
+
+    ctx = C()
+    context.set_ctx(ctx)
+    context.set_value('a', 1)
+
     assert True is True
 
 
