61fa70a033e9e9c18b411b29fa031fb76182f536
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 23:57:06 +0300

    writing policy tests

--- tests/policy/test_policy.py
+++ tests/policy/test_policy.py
@@ -2,40 +2,70 @@ import pytest
 
 
 def test_core_policy_decay():
-	assert True is True
+    assert True is True
 
 
 def test_core_policy_reset():
-	assert True is True
+    assert True is True
 
 
 def test_core_policy_use_network():
-	assert True is True
+    assert True is True
 
 
 def test_epsilon_greedy_policy_decay():
-	assert True is True
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.decay()
+
+    assert True is True
 
 
 def test_epsilon_greedy_policy_reset():
-	assert True is True
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.reset()
+
+    assert True is True
 
 
 def test_epsilon_greedy_policy_use_network():
-	assert True is True
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.use_network()
+
+    assert True is True
 
 
 def test_greedy_q_policy_decay():
-	assert True is True
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.decay()
+
+    assert True is True
 
 
 def test_greedy_q_policy_reset():
-	assert True is True
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.reset()
+
+    assert True is True
 
 
 def test_greedy_q_policy_use_network():
-	assert True is True
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.use_network()
+
+    assert True is True
 
 
 if __name__ == '__main__':
-	pytest.main([__file__])
+    pytest.main([__file__])
