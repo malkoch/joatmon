1aafa4a4805213b1b9bceee05c503354365f49e2
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 18:25:58 +0300

    creating test scripts

---
+++ tests/ai/test_ai_processor.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_r_l_processor_process_batch():
+	assert True is True
+
+
+def test_r_l_processor_process_state():
+	assert True is True
+
+
+if __name__ == '__main__':
+	pytest.main([__file__])
---
+++ tests/random/test_random.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+def test_core_random_decay():
+	assert True is True
+
+
+def test_core_random_reset():
+	assert True is True
+
+
+def test_core_random_sample():
+	assert True is True
+
+
+def test_gaussian_random_decay():
+	assert True is True
+
+
+def test_gaussian_random_reset():
+	assert True is True
+
+
+def test_gaussian_random_sample():
+	assert True is True
+
+
+def test_ornstein_uhlenbeck_decay():
+	assert True is True
+
+
+def test_ornstein_uhlenbeck_reset():
+	assert True is True
+
+
+def test_ornstein_uhlenbeck_sample():
+	assert True is True
+
+
+if __name__ == '__main__':
+	pytest.main([__file__])
