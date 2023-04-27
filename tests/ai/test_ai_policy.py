07d5fb389da921d04ce5e743829e0869b43aae44
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 20:26:04 +0300

    writing policy tests

--- tests/ai/test_ai_policy.py
+++ tests/ai/test_ai_policy.py
@@ -14,26 +14,56 @@ def test_core_policy_use_network():
 
 
 def test_epsilon_greedy_policy_decay():
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.decay()
+
     assert True is True
 
 
 def test_epsilon_greedy_policy_reset():
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.reset()
+
     assert True is True
 
 
 def test_epsilon_greedy_policy_use_network():
+    from joatmon.policy import EpsilonGreedyPolicy
+
+    policy = EpsilonGreedyPolicy()
+    policy.use_network()
+
     assert True is True
 
 
 def test_greedy_q_policy_decay():
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.decay()
+
     assert True is True
 
 
 def test_greedy_q_policy_reset():
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.reset()
+
     assert True is True
 
 
 def test_greedy_q_policy_use_network():
+    from joatmon.policy import GreedyQPolicy
+
+    policy = GreedyQPolicy()
+    policy.use_network()
+
     assert True is True
 
 
