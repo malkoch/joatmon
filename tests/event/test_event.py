089359d2c6a7ea5be27598f3e791ce2833a0f729
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-27 23:55:09 +0300

    writing event tests

--- tests/event/test_event.py
+++ tests/event/test_event.py
@@ -2,10 +2,37 @@ import pytest
 
 
 def test_async_event_fire():
+    import asyncio
+    from joatmon.event import AsyncEvent
+
+    event = AsyncEvent()
+
+    async def a():
+        ...
+
+    event += a
+
+    asyncio.wait_for(asyncio.ensure_future(event.fire()), 10)
+
+    event -= a
+
     assert True is True
 
 
 def test_event_fire():
+    from joatmon.event import Event
+
+    event = Event()
+
+    def a():
+        ...
+
+    event += a
+
+    event.fire()
+
+    event -= a
+
     assert True is True
 
 
