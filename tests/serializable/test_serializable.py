eda5351efefdb81dcd0c5de39d7c6b6d473e10c1
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-04-29 20:37:24 +0300

    Update test_serializable.py

--- tests/serializable/test_serializable.py
+++ tests/serializable/test_serializable.py
@@ -1,9 +1,21 @@
+import json
+
 import pytest
 
 
 def test_serializable_items():
     from joatmon.serializable import Serializable
     assert list(Serializable(a=12, b=13).items()) == [('a', 12), ('b', 13)]
+
+    assert repr(Serializable(a=12, b=13)) == str(Serializable(a=12, b=13))
+    assert Serializable(a=12, b=13)['a'] == 12
+    assert Serializable(a=12, b=13).dict == {'a': 12, 'b': 13}
+    assert Serializable.from_dict({'a': 12, 'b': 13})
+    assert Serializable.from_dict({'a': 12, 'b': 13}).json == json.dumps({'a': 12, 'b': 13})
+    assert Serializable.from_dict({'a': 12, 'b': 13}).pretty_json == json.dumps({'a': 12, 'b': 13}, indent=4)
+    Serializable.from_bytes(Serializable.from_dict({'a': 12, 'b': 13}).bytes)
+    assert Serializable.from_dict({'a': 12, 'b': 13}).snake
+    assert Serializable.from_dict({'a': 12, 'b': 13}).pascal
 
 
 def test_serializable_keys():
