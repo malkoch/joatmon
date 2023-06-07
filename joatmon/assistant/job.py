3f6b51075fc279b453caa8c23ea2bdbd96fc8a55
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-06-07 21:39:06 +0300

    update base classes

--- joatmon/assistant/job.py
+++ joatmon/assistant/job.py
@@ -2,27 +2,24 @@ import threading
 
 
 class BaseJob:
-    arguments = {
-
-    }
-
     def __init__(self, api, **kwargs):
         self.api = api
         self.kwargs = kwargs
         self.event = threading.Event()
         self.thread = threading.Thread(target=self.run)
 
+    @staticmethod
+    def params():
+        return []
+
     def run(self):
-        if not self.event.is_set():
-            self.event.set()
+        raise NotImplementedError
 
     def running(self):
         return not self.event.is_set()
 
     def start(self):
-        if not self.event.is_set():
-            self.thread.start()
+        self.thread.start()
 
     def stop(self):
-        if not self.event.is_set():
-            self.event.set()
+        self.event.set()
