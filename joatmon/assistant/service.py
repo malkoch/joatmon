e83cf3623ba378d895aabf643c7f99c115108ead
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-06-30 14:29:50 +0300

    core service

--- joatmon/assistant/service.py
+++ joatmon/assistant/service.py
@@ -1,4 +1,11 @@
+import dataclasses
+import enum
+import importlib.util
+import json
+import os
 import threading
+
+from joatmon.utility import first
 
 
 class BaseService:
@@ -23,3 +30,92 @@ class BaseService:
 
     def stop(self):
         self.event.set()
+
+
+class ServiceState(enum.Enum):
+    running = enum.auto()
+    finished = enum.auto()
+    stopped = enum.auto()
+
+
+@dataclasses.dataclass
+class ServiceInfo:
+    name: str
+    state: ServiceState
+    service: BaseService
+
+
+def create(api):
+    name = api.listen('name')
+    priority = api.listen('priority')
+    mode = api.listen('mode')
+    script = api.listen('script')
+    kwargs = {}
+    for k in get_class(script).params():
+        kwargs[k] = api.listen(k)
+
+    create_args = {
+        'name': name,
+        'priority': priority,
+        'mode': mode,
+        'script': script,
+        'status': True,
+        'kwargs': kwargs
+    }
+
+    settings = json.loads(open('iva.json', 'r').read())
+    services = settings.get('services', [])
+
+    services.append(create_args)
+
+    settings['services'] = services
+    open('iva.json', 'w').write(json.dumps(settings, indent=4))
+
+
+def get_class(name):
+    service = None
+
+    settings = json.loads(open('iva.json', 'r').read())
+    for scripts in settings.get('scripts', []):
+        if os.path.isabs(scripts):
+            if os.path.exists(scripts) and os.path.exists(os.path.join(scripts, f'{name}.py')):
+                spec = importlib.util.spec_from_file_location(scripts, os.path.join(scripts, f'{name}.py'))
+                action_module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(action_module)
+            else:
+                continue
+        else:
+            try:
+                _module = __import__(scripts, fromlist=[f'{name}'])
+            except ModuleNotFoundError:
+                continue
+
+            action_module = getattr(_module, name, None)
+
+        if action_module is None:
+            continue
+
+        service = getattr(action_module, 'Service', None)
+
+    return service
+
+
+def get(api, name):
+    settings = json.loads(open('iva.json', 'r').read())
+    task_info = first(filter(lambda x: x['name'] == name, settings.get('services', [])))
+
+    if task_info is None:
+        task_info = {'script': name, 'kwargs': {}}
+
+    script = task_info['script']
+
+    service = get_class(script)
+
+    if service is None:
+        api.say('service is not found')
+        return None
+
+    kwargs = {**task_info['kwargs'], 'parent_os_path': api.parent_os_path, 'os_path': api.os_path}
+
+    service = service(api, **kwargs)
+    return service
