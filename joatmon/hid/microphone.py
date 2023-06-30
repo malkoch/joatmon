da053c0a98adead61278ba1a6d98c9ec9e261291
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-06-30 14:28:22 +0300

    refactoring

--- joatmon/hid/microphone.py
+++ joatmon/hid/microphone.py
@@ -1,69 +1,62 @@
-import json
-import queue
-import threading
-
-import openai
-import speech_recognition as sr
-import speech_recognition.exceptions
-
-
 class InputDriver:
     def __init__(self, o):
         super(InputDriver, self).__init__()
 
         self.o = o
 
-        openai.api_key = json.loads(open('iva.json', 'r').read())['configs']['openai']['key']
+        # openai.api_key = json.loads(open('iva.json', 'r').read())['configs']['openai']['key']
 
-        self.r = sr.Recognizer()
-        self.r.energy_threshold = 350
-        self.r.pause_threshold = 0.8
-        self.r.dynamic_energy_threshold = False
-        self.r.timeout = 1
+        # self.r = sr.Recognizer()
+        # self.r.energy_threshold = 350
+        # self.r.pause_threshold = 0.8
+        # self.r.dynamic_energy_threshold = False
+        # self.r.timeout = 1
 
-        self.audio_queue = queue.Queue()
-        self.result_queue = queue.Queue()
+        # self.audio_queue = queue.Queue()
+        # self.result_queue = queue.Queue()
 
-        self.stop_event = threading.Event()
+        # self.stop_event = threading.Event()
 
-        self.listening_thread = threading.Thread(target=self.record_audio)
-        self.translator_thread = threading.Thread(target=self.transcribe_forever)
+        # self.listening_thread = threading.Thread(target=self.record_audio)
+        # self.translator_thread = threading.Thread(target=self.transcribe_forever)
 
-        self.listening_thread.start()
-        self.translator_thread.start()
+        # self.listening_thread.start()
+        # self.translator_thread.start()
 
-    def record_audio(self):
-        with sr.Microphone(sample_rate=16000) as source:
-            # self.r.adjust_for_ambient_noise(source)
+    # def record_audio(self):
+    #     with sr.Microphone(sample_rate=16000) as source:
+    #         # self.r.adjust_for_ambient_noise(source)
+    #
+    #         print('listening')
+    #
+    #         while not self.stop_event.is_set():
+    #             try:
+    #                 audio = self.r.listen(source, timeout=1)
+    #                 self.audio_queue.put_nowait(audio)
+    #             except speech_recognition.exceptions.WaitTimeoutError:
+    #                 ...
 
-            print('listening')
-
-            while not self.stop_event.is_set():
-                try:
-                    audio = self.r.listen(source, timeout=1)
-                    self.audio_queue.put_nowait(audio)
-                except speech_recognition.exceptions.WaitTimeoutError:
-                    ...
-
-    def transcribe_forever(self):
-        while not self.stop_event.is_set():
-            try:
-                audio = self.audio_queue.get_nowait()
-
-                result = self.r.recognize_whisper_api(audio, model='whisper-1', api_key=json.loads(open('iva.json', 'r').read())['configs']['openai']['key'])
-
-                self.result_queue.put_nowait(result)
-            except queue.Empty:
-                ...
+    # def transcribe_forever(self):
+    #     while not self.stop_event.is_set():
+    #         try:
+    #             audio = self.audio_queue.get_nowait()
+    #
+    #             result = self.r.recognize_whisper_api(audio, model='whisper-1', api_key=json.loads(open('iva.json', 'r').read())['configs']['openai']['key'])
+    #
+    #             self.result_queue.put_nowait(result)
+    #         except queue.Empty:
+    #             ...
 
     def listen(self, prompt=None):
         if prompt is not None:
             self.o.say(prompt)
 
-        ignore = ['!', '?', ',', '.']
-        # need to find a way to remove these and keep punctuations as well
-        return ''.join([c.lower() for c in self.result_queue.get().lower().strip() if c not in ignore])
-        # return input()
+        prompt = prompt or 'prompt'
+        return input(f'{prompt}: ')
+
+        # ignore = ['!', '?', ',', '.']
+        # return ''.join([c.lower() for c in self.result_queue.get().lower().strip() if c not in ignore])
 
     def stop(self):
-        self.stop_event.set()
+        # self.stop_event.set()
+        ...
