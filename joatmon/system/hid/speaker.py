925b7e62035ea1467c81fd5ba0fd2d38bf4c45ee
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-07-09 22:48:36 +0300

    update hid

--- joatmon/system/hid/speaker.py
+++ joatmon/system/hid/speaker.py
@@ -1,6 +1,17 @@
+import json
+
+from elevenlabs import (
+    generate,
+    set_api_key,
+    stream
+)
+
+
 class OutputDevice:
     def __init__(self):
         super(OutputDevice, self).__init__()
+
+        set_api_key(json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['key'])
 
     def say(self, text):
         import pyttsx3
@@ -9,3 +20,12 @@ class OutputDevice:
         engine.say(text)
         engine.runAndWait()
         engine.stop()
+
+        # audio_stream = generate(
+        #     text=text,
+        #     voice=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['voice'],
+        #     model=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['model'],
+        #     stream=True
+        # )
+#
+        # stream(audio_stream)
