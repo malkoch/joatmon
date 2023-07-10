85b8bf647b9b203bbbcc4dadc65594666e76be99
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-07-10 20:39:05 +0300

    caching in tts

--- joatmon/system/hid/speaker.py
+++ joatmon/system/hid/speaker.py
@@ -1,9 +1,10 @@
 import json
+import os
 
 from elevenlabs import (
     generate,
-    set_api_key,
-    stream
+    play,
+    set_api_key
 )
 
 
@@ -14,18 +15,29 @@ class OutputDevice:
         set_api_key(json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['key'])
 
     def say(self, text):
-        import pyttsx3
-        engine = pyttsx3.init()
-        engine.setProperty('rate', 175)
-        engine.say(text)
-        engine.runAndWait()
-        engine.stop()
+        text_hash = ''.join([c.lower() for c in text.lower().strip() if c not in ['!', '?', ',', '/', '\\']]).replace(' ', '_').replace('.', '_').replace(':', '_')
+        text_audio_path = os.path.join('cache', f'{text_hash}.mp3')
 
-        # audio_stream = generate(
-        #     text=text,
-        #     voice=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['voice'],
-        #     model=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['model'],
-        #     stream=True
-        # )
-#
-        # stream(audio_stream)
+        if not os.path.exists(os.path.dirname(text_audio_path)):
+            os.makedirs(os.path.dirname(text_audio_path))
+
+        if os.path.exists(text_audio_path):
+            audio = open(text_audio_path, 'rb').read()
+            play(audio)
+        else:
+            try:
+                audio = generate(
+                    text=text,
+                    voice=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['voice'],
+                    model=json.loads(open('iva.json', 'r').read())['config']['elevenlabs']['model']
+                )
+                open(text_audio_path, 'wb').write(audio)
+                play(audio)
+            except Exception as ex:
+                print(str(ex))
+                import pyttsx3
+                engine = pyttsx3.init()
+                engine.setProperty('rate', 175)
+                engine.say(text)
+                engine.runAndWait()
+                engine.stop()
