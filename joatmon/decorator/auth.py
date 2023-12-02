bb0addbb3e7b78c98624981be76f090a789de25e
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2023-12-02 17:50:59 +0300

    Update auth.py

--- joatmon/decorator/auth.py
+++ joatmon/decorator/auth.py
@@ -20,7 +20,7 @@ def authorized(auth, token, issuer):  # use current token and issuer
             token_value = context.get_value(token).get()
             issuer_value = context.get_value(issuer).get()
 
-            audience = f'{func.__module__}.{func.__qualname__}'
+            audience = '.'.join(f'{func.__module__}.{func.__qualname__}'.split('.')[-2:])
 
             authorizer = context.get_value(auth)
             await authorizer.authorize(token_value, issuer_value, audience)
