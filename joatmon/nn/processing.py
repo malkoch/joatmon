4dffd9b32028a938ac7c863c7db89906f3a1f81d
Hamitcan MALKOÇ <hamitcanmalkoc@gmail.com>
2024-01-20 01:15:09 +0300

    doc string for nn module

--- joatmon/nn/processing.py
+++ joatmon/nn/processing.py
@@ -2,13 +2,31 @@ import numpy as np
 
 
 class OneHotEncoder:
+    """
+    This class is used for performing one-hot encoding on categorical data.
+
+    Attributes:
+        _data (dict): A dictionary to store the one-hot encoded representation of each category.
+        _reverse_data (dict): A dictionary to store the original category for each one-hot encoded representation.
+        _eye (numpy.ndarray): An identity matrix used for one-hot encoding.
+    """
+
     def __init__(self):
+        """
+        Initializes the OneHotEncoder class.
+        """
         self._data = {}
         self._reverse_data = {}
 
         self._eye = None
 
     def fit(self, data):
+        """
+        Fits the encoder to the data.
+
+        Args:
+            data (list): The data to fit the encoder to.
+        """
         length = len(data)
 
         self._eye = np.eye(length)
@@ -20,14 +38,52 @@ class OneHotEncoder:
                 self._reverse_data[idx] = row
 
     def transform(self, data):
+        """
+        Transforms the data into one-hot encoded format.
+
+        Args:
+            data (list): The data to transform.
+
+        Returns:
+            list: The one-hot encoded data.
+        """
         return [self._data[row] for row in data]
 
     def inverse_transform(self, data):
+        """
+        Transforms the one-hot encoded data back into its original format.
+
+        Args:
+            data (list): The one-hot encoded data to transform.
+
+        Returns:
+            list: The original data.
+        """
         return [self._reverse_data[self._eye.tolist().index(row)] for row in data]
 
 
 class Tokenizer:
+    """
+    This class is used for tokenizing text data.
+
+    Attributes:
+        num_words (int): The maximum number of words to keep, based on word frequency.
+        document_count (int): The total number of documents (texts) that the tokenizer was trained on.
+        word_counts (dict): A dictionary of words and their counts.
+        word_docs (dict): A dictionary of words and how many documents each appeared in.
+        oov_token (str): The out of vocabulary token.
+        word_index (dict): A dictionary of words and their uniquely assigned integers.
+        index_word (dict): A dictionary of integers and their corresponding words.
+        index_docs (dict): A dictionary of integers and how many documents the corresponding word appeared in.
+    """
+
     def __init__(self, num_words=1000):
+        """
+        Initializes the Tokenizer class.
+
+        Args:
+            num_words (int, optional): The maximum number of words to keep, based on word frequency. Only the most common `num_words-1` words will be kept. Defaults to 1000.
+        """
         self.num_words = num_words
         self.document_count = 0
         self.word_counts = {}
@@ -38,6 +94,12 @@ class Tokenizer:
         self.index_docs = {}
 
     def fit_on_text(self, data):
+        """
+        Fits the tokenizer to the data.
+
+        Args:
+            data (list): The data to fit the tokenizer to.
+        """
         for text in data:
             self.document_count += 1
             for w in text:
@@ -72,6 +134,15 @@ class Tokenizer:
             self.index_docs[self.word_index[w]] = c
 
     def text_to_sequence(self, data):
+        """
+        Transforms the data into a sequence of integers.
+
+        Args:
+            data (list): The data to transform.
+
+        Yields:
+            list: The transformed data.
+        """
         num_words = self.num_words
         oov_token_index = self.word_index.get(self.oov_token)
         for text in data:
@@ -89,6 +160,15 @@ class Tokenizer:
             yield vect
 
     def sequence_to_text(self, data):
+        """
+        Transforms the sequence of integers back into text.
+
+        Args:
+            data (list): The sequence of integers to transform.
+
+        Yields:
+            str: The transformed text.
+        """
         num_words = self.num_words
         oov_token_index = self.word_index.get(self.oov_token)
         for seq in data:
@@ -108,6 +188,15 @@ class Tokenizer:
 
 
 def word_tokenize(data):
+    """
+    Tokenizes the text into words.
+
+    Args:
+        data (str): The text to tokenize.
+
+    Returns:
+        list: The tokenized text.
+    """
     return data.split(' ')
 
 
@@ -119,6 +208,20 @@ def pad_sequences(
         truncating="pre",
         value=0.0,
 ):
+    """
+    Pads sequences to the same length.
+
+    Args:
+        sequences (list): List of sequences (each sequence is a list of integers).
+        maxlen (int, optional): Maximum length for all sequences. If not provided, sequences will be padded to the length of the longest individual sequence.
+        dtype (str, optional): Type of the output sequences. Defaults to 'int32'.
+        padding (str, optional): 'pre' or 'post': pad either before or after each sequence. Defaults to 'pre'.
+        truncating (str, optional): 'pre' or 'post': remove values from sequences larger than `maxlen`, either at the beginning or at the end of the sequences. Defaults to 'pre'.
+        value (float, optional): Value to pad the sequences to the desired value. Defaults to 0.0.
+
+    Returns:
+        numpy.ndarray: Padded sequences.
+    """
     num_samples = len(sequences)
 
     lengths = []
