6edcd508ed79ab7b60afbdf5228a7bcafdfcd3f7
Hamitcan MALKOÇ <hamitcanmalkoc@appyfany.com>
2024-07-16 19:35:43 +0300

    nn concat unit test

--- tests/test_nn_functional.py
+++ tests/test_nn_functional.py
@@ -6,3 +6,15 @@ def test_pad():
 
     assert zero.shape == (2, 2, 2, 2)
     assert padded.shape == (2, 2, 4, 4)
+
+
+def test_concat():
+    from joatmon.nn.functional import concat, zeros, ones
+
+    zero = zeros((2, 2))
+    one = ones((2, 2))
+    concated_4x2 = concat([zero, one], axis=0)
+    concated_2x4 = concat([zero, one], axis=1)
+
+    assert concated_4x2.shape == (4, 2)
+    assert concated_2x4.shape == (2, 4)
