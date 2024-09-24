3b19b4d560b2949896a6f2249e92b224e1cfc1f6
Hamitcan MALKOÇ <hamitcanmalkoc@appyfany.com>
2024-09-24 20:15:11 +0300

    host path

--- joatmon/system/fs.py
+++ joatmon/system/fs.py
@@ -11,72 +11,72 @@ class FileSystemModule(Module):
         self._root = root
         self._cwd = '/'
 
-    def __get_host_path(self, path: str):
+    def _get_host_path(self, path: str):
         if os.path.isabs(path):
             return os.path.abspath(os.path.join(self._root, path))
         else:
             return os.path.abspath(os.path.join(os.path.abspath(self._root), './' + os.path.abspath(os.path.join(self._cwd, path))))
 
-    def __get_system_path(self, path: str):
+    def _get_system_path(self, path: str):
         if os.path.isabs(path):
             return path.replace(os.path.commonpath((self._root, path)), '', 1)
         else:
             return path
 
     def stat(self, path: str):
-        size, unit = convert_size(os.path.getsize(self.__get_host_path(path))).split(' ')
+        size, unit = convert_size(os.path.getsize(self._get_host_path(path))).split(' ')
         return {
             'size': size,
             'unit': unit
         }
 
     def touch(self, file: str):
-        with open(self.__get_host_path(file), 'w'):
+        with open(self._get_host_path(file), 'w'):
             pass
 
     def write(self, file: str, content: str):
-        with open(self.__get_host_path(file), 'w') as f:
+        with open(self._get_host_path(file), 'w') as f:
             f.write(content)
 
     def append(self, file: str, content: str):
-        with open(self.__get_host_path(file), 'a') as f:
+        with open(self._get_host_path(file), 'a') as f:
             f.write(content)
 
     def read(self, file: str):
-        with open(self.__get_host_path(file), 'r') as f:
+        with open(self._get_host_path(file), 'r') as f:
             return f.read()
 
     def ls(self, path: str):
-        content = list(os.listdir(self.__get_host_path(path)))
-        content.sort(key=lambda x: (not x.startswith('.'), not os.path.isdir(os.path.join(self.__get_host_path(path), x)), x))
+        content = list(os.listdir(self._get_host_path(path)))
+        content.sort(key=lambda x: (not x.startswith('.'), not os.path.isdir(os.path.join(self._get_host_path(path), x)), x))
 
         for x in content:
             yield x, self.stat(os.path.join(path, x))
 
-    def cd(self, path: str):
+    def cd(self, path: str):  # need to check if path is mapped to host system
         if os.path.isabs(path):
             self._cwd = path
         else:
             self._cwd = os.path.abspath(os.path.join(self._cwd, path))
 
     def mkdir(self, path: str):
-        path = self.__get_host_path(path)
+        path = self._get_host_path(path)
 
         if not os.path.exists(path):
             os.mkdir(path)
 
     def rm(self, path: str):
-        path = self.__get_host_path(path)
+        path = self._get_host_path(path)
         if os.path.isdir(path):
             os.rmdir(path)
         elif os.path.isfile(path):
             os.remove(path)
 
     def exists(self, path: str):
-        return os.path.exists(self.__get_host_path(path))
+        return os.path.exists(self._get_host_path(path))
 
     def isdir(self, path: str):
-        return os.path.isdir(self.__get_host_path(path))
+        return os.path.isdir(self._get_host_path(path))
 
     def isfile(self, path: str):
-        return os.path.isfile(self.__get_host_path(path))
+        return os.path.isfile(self._get_host_path(path))
