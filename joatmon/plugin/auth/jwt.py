ebb312d182c82706e5743ba16799b8627c089ab1
Hamitcan MALKOÇ <hamitcanmalkoc@appyfany.com>
2024-11-24 12:00:15 +0300

    auth module -> remove issuer as parameter

--- joatmon/plugin/auth/jwt.py
+++ joatmon/plugin/auth/jwt.py
@@ -14,21 +14,21 @@ class JWTAuth(Auth):
 
     """
 
-    def __init__(self, secret: str):
+    def __init__(self, issuer: str, secret: str):
         """
         Initialize JWTAuth with the given secret key.
 
         Args:
             secret (str): The secret key used for encoding and decoding the JWT.
         """
+        self.issuer = issuer
         self.secret = secret
 
-    async def authenticate(self, issuer, audience, expire_at, **kwargs):
+    async def authenticate(self, audience, expire_at, **kwargs):
         """
         Authenticate a user by encoding a JWT with the given parameters.
 
         Args:
-            issuer (str): The issuer of the authentication.
             audience (str): The audience of the authentication.
             expire_at (datetime): The expiration date of the authentication.
             **kwargs: Additional claims to be included in the JWT.
@@ -36,7 +36,7 @@ class JWTAuth(Auth):
         Returns:
             str: The encoded JWT.
         """
-        kwargs.update({'exp': expire_at, 'iss': issuer, 'aud': audience})
+        kwargs.update({'exp': expire_at, 'iss': self.issuer, 'aud': audience})
 
         token = jwt.encode(
             payload=kwargs,
@@ -45,13 +45,12 @@ class JWTAuth(Auth):
         )
         return token
 
-    async def authorize(self, token, issuer, audience):
+    async def authorize(self, token, audience):
         """
         Authorize a user by decoding the given JWT and verifying the issuer and audience.
 
         Args:
             token (str): The JWT to be decoded.
-            issuer (str): The expected issuer of the JWT.
             audience (str): The expected audience of the JWT.
 
         Returns:
@@ -61,8 +60,8 @@ class JWTAuth(Auth):
             CoreException: If the JWT cannot be decoded or the issuer and audience do not match the expected values.
         """
         try:
-            decoded = jwt.decode(token, self.secret, issuer=issuer, audience=audience, algorithms='RS256')
-        except (jwt.PyJWTError, ValueError):
+            decoded = jwt.decode(token, self.secret, issuer=self.issuer, audience=audience, algorithms='RS256')
+        except (jwt.PyJWTError, ValueError) as ex:
             raise CoreException('not_authorized')
 
         return decoded
